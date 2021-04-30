56df1d8458f14137680d2cd9728db24a63464c02
rjs595959 <rjs595959@naver.com>
2021-04-30 12:21:03 +0900

    defense file download by using encodeURI

--- mall-backend/src/images/index.js
+++ mall-backend/src/images/index.js
@@ -4,8 +4,12 @@ import fs from 'fs';
 const images = new Router();
 
 images.get('/:fileName', ctx => {
-    let fileName = ctx.params.fileName;
-    ctx.body = fs.readFileSync('./images/'+fileName);
+    let fileName = decodeURI(ctx.params.fileName);
+    if(fileName.indexOf('..') != -1) {
+        ctx.throw(401, '허락되지 않는 접근입니다.');
+    } else {
+        ctx.body = fs.readFileSync('./images/'+fileName);
+    }
 })
 
 export default images;
